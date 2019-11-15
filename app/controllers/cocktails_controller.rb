f3249a47d9b9fff8af0876305382550450b62664
Tanguy <blondiaux.tanguy@gmail.com>
2019-11-15 15:28:16 +0100

    Active storage setup

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -9,17 +9,18 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
+    @cocktail.save!
     redirect_to cocktails_path
   end
 
   def show
     @cocktail = Cocktail.find(params[:id])
+
   end
 
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
